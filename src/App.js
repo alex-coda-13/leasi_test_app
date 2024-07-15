5ef6b6d6d51bcc47ca497650c3ca3499f3252e39
alexandre codaccioni <alexandre.codaccioni@gmail.com>
2024-07-15 11:50:33 +0200

    init test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+
+
+// https://leasiappv2bucket145309-dev.s3.eu-west-1.amazonaws.com/public/images/catalogue/000badc0-bd92-4f05-9467-ce585533de99/1.webp
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-blue-500 text-white min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome to My Single Page React App</h1>
+        <p className="mt-4 text-lg">This is a simple React app with a single page.</p>
       </header>
     </div>
   );
